b2e1cbcfdb33774bda9738d7b704f39d7a6916f2
sp1603 <113911053+sp1603@users.noreply.github.com>
2024-11-02 11:20:34 -0400

    fix directory

--- src/components/subcomponents/ProviderInfo.tsx
+++ src/components/subcomponents/ProviderInfo.tsx
@@ -97,6 +97,18 @@ const directoryData =
             details: "bob@gmail.com",
             image: "https://images.squarespace-cdn.com/content/v1/54822a56e4b0b30bd821480c/45ed8ecf-0bb2-4e34-8fcf-624db47c43c8/Golden+Retrievers+dans+pet+care.jpeg",
         },
+        {
+            name: "bob",
+            description: "firefighter",
+            details: "bob@gmail.com",
+            image: "https://images.squarespace-cdn.com/content/v1/54822a56e4b0b30bd821480c/45ed8ecf-0bb2-4e34-8fcf-624db47c43c8/Golden+Retrievers+dans+pet+care.jpeg",
+        },
+        {
+            name: "bob",
+            description: "firefighter",
+            details: "bob@gmail.com",
+            image: "https://images.squarespace-cdn.com/content/v1/54822a56e4b0b30bd821480c/45ed8ecf-0bb2-4e34-8fcf-624db47c43c8/Golden+Retrievers+dans+pet+care.jpeg",
+        }
 ];
 const eventInfo = {
     title: "Tour Our Station",
