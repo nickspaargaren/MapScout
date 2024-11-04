ec74631856b9b5fb04c1315e0f126c396d0bb115
Athanasios Taprantzis <thatapras@hotmail.gr>
2024-11-03 23:34:08 -0500

    UI fixes

--- src/components/subcomponents/ProviderInfo.tsx
+++ src/components/subcomponents/ProviderInfo.tsx
@@ -1,8 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { FaRegClock } from "react-icons/fa";
-import { SlGlobe } from "react-icons/sl";
-import { TbPlaystationCircle } from "react-icons/tb";
-import { IoPhonePortraitOutline } from "react-icons/io5";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -19,6 +15,7 @@ import Directory from "components/dashboard/Directory";
 import EmbedForm from "components/dashboard/embed-component/EmbedForm";
 import EmbedComponent from "components/dashboard/embed-component/EmbedComponent";
 import EventInfoComponent from "components/dashboard/EventInfoComponent";
+import GeneralInfo from "components/dashboard/GeneralInfo";
 
 {
     /*TO BE DELETED */
@@ -155,17 +152,8 @@ const ProviderInfo = (props) => {
     }
 
     const categoriesToUse = props.categories || [];
-    const iconStyle = {
-        marginRight: "20px",
-        verticalAlign: "middle",
-        color: "#226DFF",
-    };
 
-    const infoStyle = {
-        display: "flex",
-        alignItems: "center",
-        marginBottom: "10px",
-    };
+    
 
     return (
         <Container fluid className="provider-info-container">
@@ -178,118 +166,21 @@ const ProviderInfo = (props) => {
             </Row>
             {/* Sample components that in the future should be added dynamically
             based on the response from firebase */}
-            <Collapsible
-                label={"General Info"}
-                style={{
-                    maxWidth: "1000px",
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                }}
-                containerStyle={{ placeItems: "flex-start", }}
-            >
-                <Row className="info-rows" style={{ justifyContent: "flex-start", maxWidth: "100%" }}>
-                    <Col md={12}>
-                        <div className="description-box">
-                            {props.item.description !== undefined && (
-                                <p
-                                    style={{
-                                        color: "rgba(148, 142, 142, 0.9)",
-                                        fontFamily: "Inter",
-                                        fontWeight: "500",
-                                        fontSize: "16px",
-                                        lineHeight: "19px"
-                                    }}
-                                >
-                                    {props.item.description}
-                                </p>
-                            )}
-                        </div>
-                    </Col>
-                    <Col md={12}>
-                        <div style={infoStyle}>
-                            <IoPhonePortraitOutline size={20} style={iconStyle} />
-                            <div>
-                                {" "}
-                                {props.item.phoneNum &&
-                                    props.item.phoneNum.join(", ")}
-                            </div>
-                        </div>
-                        <div style={infoStyle}>
-                            {props.item.website && props.item.website[0] && (
-                                <>
-                                    {props.item.website[0].startsWith("http") ? (<>
-                                        <SlGlobe size={20} style={iconStyle} />
-                                        <div>
-                                            <a
-                                                href={props.item.website[0]}
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                            >
-                                                p{props.item.website[0]}
-                                            </a>
-                                        </div>
-                                    </>) : (<>
-                                        <SlGlobe size={20} style={iconStyle} />
-                                        <div>
-                                            <a
-                                                href={'//' + props.item.website[0]}
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                            >
-                                                {props.item.website[0]}
-                                            </a>
-                                        </div></>)}
-
-                                </>
-                            )}
-                        </div>
-                        <div style={infoStyle}>
-                            <TbPlaystationCircle size={20} style={iconStyle} />
-                            <div>
-                                {" "}
-                                {props.item.address
-                                    .toString()
-                                    .split(",")
-                                    .map((value, index) => {
-                                        if (index === 0) {
-                                            return (
-                                                <div style={{ display: "inline" }}>
-                                                    {value},
-                                                </div>
-                                            );
-                                        }
-                                        if (
-                                            index ===
-                                            props.item.address.toString().split(",")
-                                                .length -
-                                            1
-                                        ) {
-                                            return (
-                                                <div style={{ display: "inline" }}>
-                                                    {value}
-                                                </div>
-                                            );
-                                        }
-                                        if (index === 1) {
-                                            return (
-                                                <div
-                                                    style={{ display: "inline" }}
-                                                >{`${value},`}</div>
-                                            );
-                                        }
-                                        return `${value},`;
-                                    })}
-                            </div>
-                        </div>
-                        <div style={infoStyle}>
-                            <FaRegClock size={20} style={iconStyle} />
-                            <div className="modal-hours-container">
-                                {props.item.hours && calculateHours(props)}
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
-            </Collapsible>
+            <Row className="info-rows">
+                <Col md={12}>
+                    <Collapsible
+                        label={"General Info"}
+                        style={{
+                            maxWidth: "1000px",
+                            marginLeft: "auto",
+                            marginRight: "auto",
+                        }}
+                        containerStyle={{ placeItems: "flex-start", }}
+                    >
+                        <GeneralInfo item={props.item} />
+                    </Collapsible>
+                </Col>
+            </Row>
             {/* Sample components that in the future should be added dynamically
             based on the response from firebase */}
             <Row className="info-rows" >
@@ -399,124 +290,6 @@ const ProviderInfo = (props) => {
         </Container>
     );
 };
-
-function calculateHours(props) {
-    const rows = [];
-    const startandFinish = [0]; // In pairs, keep track of the starting and ending days with same time
-    const days = [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday",
-    ];
-    const abbrevDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-    for (let i = 1; i < 7; i++) {
-        // not both undefined
-        if (props.item.hours[days[i]] !== props.item.hours[days[i - 1]]) {
-            if (
-                !props.item.hours[days[i]] ||
-                !props.item.hours[days[i - 1]] ||
-                props.item.hours[days[i]][0] !==
-                props.item.hours[days[i - 1]][0] ||
-                props.item.hours[days[i]][1] !==
-                props.item.hours[days[i - 1]][1]
-            ) {
-                startandFinish.push(i - 1);
-                startandFinish.push(i);
-            }
-        }
-        if (i === 6) {
-            startandFinish.push(6);
-        }
-    }
-    for (let i = 0; i < startandFinish.length; i += 2) {
-        const children = [];
-        if (startandFinish[i] === startandFinish[i + 1]) {
-            children.push(
-                <Col className="modal-col-flex-end" sm={5}>
-                    {days[startandFinish[i]]}
-                </Col>,
-            );
-        } else {
-            const subchild = [
-                <div>
-                    {abbrevDays[startandFinish[i]]} -{" "}
-                    {abbrevDays[startandFinish[i + 1]]}
-                </div>,
-            ];
-            children.push(
-                <Col className="modal-col-flex-end" sm={5}>
-                    {subchild}
-                </Col>,
-            );
-        }
-        children.push(
-            <Col className="modal-col-flex-start">
-                {props.item.hours[days[startandFinish[i]]]
-                    ? props.item.hours[days[startandFinish[i]]].map(
-                        (time, index) =>
-                            formatTime(
-                                props.item.hours[days[startandFinish[i]]],
-                                time,
-                                index,
-                            ),
-                    )
-                    : "CLOSED"}
-            </Col>,
-        );
-        rows.push(<Row>{children}</Row>);
-    }
-    return rows;
-
-    //   <Row>
-    //   <Col className="modal-col-flex-end" sm={5}>
-    //     <div>
-    //         Monday
-    //     </div>
-    //   </Col>
-    //   <Col className="modal-col-flex-start">
-    //     <div>
-    //       {' '}
-    //       {props.item.hours.Monday ? props.item.hours.Monday.map((time, index) => formatTime(props.item.hours.Monday, time, index)) : 'CLOSED'}
-    //     </div>
-    //   </Col>
-    // </Row>
-}
-
-function formatTime(arr, time, index) {
-    if (time == null) {
-        if (index !== arr.length - 1) {
-            return <div className="modal-text">CLOSED - </div>;
-        }
-        return <div className="modal-text">CLOSED</div>;
-    }
-    const seconds = time;
-    let hours = Math.floor(seconds / 3600);
-    let mins: string = ((seconds / 60) % 60).toString();
-    const endtime_ending = hours < 12 ? "AM" : "PM";
-    hours %= 12;
-    if (hours === 0) {
-        hours = 12;
-    }
-    if (parseInt(mins) < 10) {
-        mins = `0${mins}`;
-    }
-    // time = Math.round(time/36);  //
-    // if (time/100 > 12) { //check if hour
-    //   time = time - 1200;
-    //   endtime_ending = "PM";
-    // }
-    // let timestr = time.toString()
-    // let timeformat = timestr.substring(0, timestr.length - 2) + ":" + timestr.substring(timestr.length - 2) + endtime_ending;
-    const timeformat = `${hours}:${mins}${endtime_ending}`;
-    if (index !== arr.length - 1) {
-        return <div className="modal-text">{timeformat} - </div>;
-    }
-    return <div className="modal-text">{timeformat}</div>;
-}
 
 export default compose<any>(
     withFirestore,
